a01abe914467816cf42662f459dabfd2d6ff9bb4
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-02-22 14:34:28 +0300

    Completed assignment

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,7 @@
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[6.1]
+    def change
+        # change_column :table_name, :column_name, :type
+        change_column :students, :birthdate, :datetime
+    end
+  end
+  
